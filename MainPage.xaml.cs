9b0b95e289c5c95f9f9dc536b777137b3e217b9a
Joel <124709176+JOELMAMANIUMIRI@users.noreply.github.com>
2025-04-11 18:32:28 -0300

    Add files via upload
    primeiro commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace Coinflip
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FlipButton_Clicked(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int sorteio = random.Next(2);
+             
+            if(sorteio == 0)
+            {
+                MoedaImg.Source = "cara.jpg";
+            }
+            else
+            {
+                MoedaImg.Source = "coroa.jpg";
+            }
+
+
+            if (SelecaoPicker.SelectedIndex == sorteio)
+            {
+                DisplayAlert("Parabens", "você venceu!", "Ok");
+            }
+            else
+            {
+                DisplayAlert("Se ferrou", "Você perdeu!", "Ok");
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+        
+    }
+
+}
